769580f6e685d94d803f324a0cbf2979bbd6e142
ARNAB CHAULYA <139354385+arnab1616@users.noreply.github.com>
2024-08-11 01:53:36 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import testRoutes from './routes/test.js'
 import visitorRoutes from './routes/visitor.js'
+import visitorMessageRoutes from './routes/message.js'
 
 const app = express();
 const port = 3000;
@@ -9,7 +10,8 @@ app.use(express.json());
 app.use(cors());
 
 app.use(testRoutes);
-app.use(visitorRoutes)
+app.use(visitorRoutes);
+app.use(visitorMessageRoutes);
 
 app.listen(port, ()=>{
     console.log("Server running on port -> ", port);
